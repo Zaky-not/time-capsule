f15be96c9d489813093644fde89146d76dcf17d7
Zaky-not <zakyyxd123@gmail.com>
2026-09-13 15:40:27 +0700

    fix member image imports

--- src/data/members.js
+++ src/data/members.js
@@ -1,3 +1,4 @@
+import abiyu from '../assets/members/abiyu.jpg';
 import abizar from '../assets/members/abizar.jpeg';
 import abyan from '../assets/members/abyan.jpeg';
 import ading from '../assets/members/ading.jpg';
@@ -9,38 +10,36 @@ import asa from '../assets/members/asa.jpg';
 import athaya from '../assets/members/athaya.jpg';
 import evan from '../assets/members/evan.png';
 import haikal from '../assets/members/haikal.jpg';
+import hapis from '../assets/members/hapis.jpeg';
 import ibra from '../assets/members/ibra.jpg';
+import ican from '../assets/members/ican.jpg';
 import igoy from '../assets/members/igoy.jpeg';
+import indra from '../assets/members/indra.jpg';
+import jaki from '../assets/members/jaki.jpeg';
 import kalip from '../assets/members/kalip.png';
 import kemas from '../assets/members/kemas.jpeg';
-import abiyu from '../assets/members/abiyu.jpg';
 import nabilmr from '../assets/members/nabilmr.jpeg';
 import nadip from '../assets/members/nadip.jpeg';
 import oka from '../assets/members/oka.jpg';
-import padil from  '../assets/members/padil.jpg';
+import padil from '../assets/members/padil.jpg';
 import pardan from '../assets/members/pardan.jpg';
 import pardas from '../assets/members/pardas.jpg';
 import raja from '../assets/members/raja.jpeg';
+import syafiq from '../assets/members/syafiq.jpeg';
 import tora from '../assets/members/tora.jpg';
 import udin from '../assets/members/udin.jpeg';
 import wibi from '../assets/members/wibi.jpeg';
 import yad from '../assets/members/yad.jpg';
 import yamin from '../assets/members/yamin.png';
 import zuhdi from '../assets/members/zuhdi.jpeg';
-import syafiq from '../assets/members/syafiq.jpeg';
-import indra from '../assets/members/indra.jpg';
-import jaki from '../assets/members/jaki.jpeg';
-import ican from '../assets/members/ican.jpg';
-//import nabil from '../assets/members/nabil.jpg';
-//import farhat from '../assets/members/farhat.jpg';
 
 const members = [
   {
     id: 'member-01',
     number: 1,
-    name: 'Muhamad Abizar ',
+    name: 'Muhamad Abizar',
     instagram: '@bie_jack15',
-    photo: '/src/assets/members/abizar.jpeg',
+    photo: abizar,
     quote: 'apabila sesuatu yang kau senangi tidak terjadi, maka senangilah apa yang terjadi..',
   },
   {
@@ -48,23 +47,23 @@ const members = [
     number: 2,
     name: 'Abyan Nadra Wijaya',
     instagram: '@abyanndra_',
-    photo: '/src/assets/members/abyan.jpeg',
+    photo: abyan,
     quote: 'Start with Bismillah.',
   },
   {
     id: 'member-03',
     number: 3,
-    name: 'Ahmad Fadhil Alhadi ',
+    name: 'Ahmad Fadhil Alhadi',
     instagram: '@fadhil.alhd',
-    photo: '/src/assets/members/ading.jpg',
+    photo: ading,
     quote: 'Jaga aku dengan taqwamu,agar aku terhindar dari fitnah yang mencabut imanku.',
   },
   {
     id: 'member-04',
     number: 4,
-    name: 'Muhammad Fatih Alfath ',
+    name: 'Muhammad Fatih Alfath',
     instagram: '@gearandgroove_',
-    photo: '/src/assets/members/alpat.jpeg',
+    photo: alpat,
     quote: 'Berdamai aja sama apapun yg terjadi,biarkan waktu yg menjawab.',
   },
   {
@@ -72,7 +71,7 @@ const members = [
     number: 5,
     name: 'M.Hazimulafdhal mufid',
     instagram: '@afdlmfid12_',
-    photo: '/src/assets/members/apdal.jpeg',
+    photo: apdal,
     quote: 'awan tidak pernah bertanya kemana angin membawanya, tapi ia tetap indah dalam perjalanannya..',
   },
   {
@@ -80,7 +79,7 @@ const members = [
     number: 6,
     name: 'M.afiq al gafar',
     instagram: '@mhmdafqalgfr',
-    photo: '/src/assets/members/apiq.jpeg',
+    photo: apiq,
     quote: 'ada waktunya belum masanya.',
   },
   {
@@ -88,7 +87,7 @@ const members = [
     number: 7,
     name: 'Ahmad Khairil Umam',
     instagram: '@_ahmadkhairill',
-    photo: '/src/assets/members/aril.jpeg',
+    photo: aril,
     quote: 'just do it.',
   },
   {
@@ -96,7 +95,7 @@ const members = [
     number: 8,
     name: 'Asadina Muhammad',
     instagram: '@lyra_saaa',
-    photo: '/src/assets/members/asa.jpg',
+    photo: asa,
     quote: '"Pohon yang besar dan tahan badai juga lahir dari benih kecil yang rentan".',
   },
   {
@@ -104,7 +103,7 @@ const members = [
     number: 9,
     name: 'Athaya Nabil Syahputra',
     instagram: '@athaya_nbl',
-    photo: '/src/assets/members/athaya.jpg',
+    photo: athaya,
     quote: 'I love being reclusive and I love being with you.',
   },
   {
@@ -112,23 +111,23 @@ const members = [
     number: 10,
     name: 'M Evan Danendra',
     instagram: '@evndanendra',
-    photo: '/src/assets/members/evan.png',
+    photo: evan,
     quote: 'It doesnt matter how slowly you move as long as you dont stop moving, something will come your way.',
   },
   {
     id: 'member-11',
     number: 11,
-    name: 'Haikal Hanif Sivanto ',
+    name: 'Haikal Hanif Sivanto',
     instagram: '@haikalhanif_S',
-    photo: '/src/assets/members/haikal.jpg',
+    photo: haikal,
     quote: 'Hidup cuman sekali, jangan banyak takutnya.',
   },
   {
     id: 'member-12',
     number: 12,
-    name: 'Muhammad hafiz ',
+    name: 'Muhammad hafiz',
     instagram: '@hfz_637',
-    photo: '/src/assets/members/hapis.jpeg',
+    photo: hapis,
     quote: 'Dont watch the clock; do what it does. Keep going.',
   },
   {
@@ -136,7 +135,7 @@ const members = [
     number: 13,
     name: 'Ibra Syafiq El-Timing',
     instagram: '@ibrasyafiqq_',
-    photo: '/src/assets/members/ibra.jpg',
+    photo: ibra,
     quote: 'jangan menunda kesempatan,Hari esok diciptakan untuk memperbaiki kesalahan ,bukan mengulanginya.',
   },
   {
@@ -144,23 +143,23 @@ const members = [
     number: 14,
     name: 'Muhammad Ihsan',
     instagram: '@ihsannmhmd',
-    photo: '/src/assets/members/igoy.jpeg',
+    photo: igoy,
     quote: 'ben iso idup senang, yo kudu mesti berjuang.',
   },
   {
     id: 'member-15',
     number: 15,
-    name: 'Khalif Rafif Nugraha ',
+    name: 'Khalif Rafif Nugraha',
     instagram: '@buat_simpel',
-    photo: '/src/assets/members/kalip.png',
-    quote: 'And sometimes, Spider-man has to do the hard thing. Even if it break Peter parkers heart  .',
+    photo: kalip,
+    quote: 'And sometimes, Spider-man has to do the hard thing. Even if it break Peter parkers heart.',
   },
   {
     id: 'member-16',
     number: 16,
     name: 'kms ramadhan nur uwais alqarni',
     instagram: '@kmsrmdhnn_',
-    photo: '/src/assets/members/kemas.jpeg',
+    photo: kemas,
     quote: 'mantul mantap betul.',
   },
   {
@@ -168,7 +167,7 @@ const members = [
     number: 17,
     name: 'Nabil maula rahman',
     instagram: '@0129.am',
-    photo: '/src/assets/members/nabilmr.jpeg',
+    photo: nabilmr,
     quote: 'itulah pokoknyo.',
   },
   {
@@ -176,7 +175,7 @@ const members = [
     number: 18,
     name: 'Ahsyar Nadhif Islamy',
     instagram: '@ahsyarnadhif',
-    photo: '/src/assets/members/nadip.jpeg',
+    photo: nadip,
     quote: 'jangan pernah menyerah, karena di balik kesusahan ada kemudahan.',
   },
   {
@@ -184,15 +183,15 @@ const members = [
     number: 19,
     name: 'Wahyu Kumala Putra',
     instagram: '@uwhy_kaa1',
-    photo: '/src/assets/members/oka.jpg',
+    photo: oka,
     quote: 'Jangan merasa dirimu tidak berguna. Bahkan debu saja diciptakan untuk tayammum.',
   },
   {
     id: 'member-20',
     number: 20,
-    name: 'Muhammad Fadhil Abdillah ',
+    name: 'Muhammad Fadhil Abdillah',
     instagram: '@fdhil_52',
-    photo: '/src/assets/members/padil.jpg',
+    photo: padil,
     quote: 'TIDAK ADA KATA LIBUR BAGI ORANG BODOH.',
   },
   {
@@ -200,7 +199,7 @@ const members = [
     number: 21,
     name: 'Fardhan bagja wiguna',
     instagram: '@fardhaa_n',
-    photo: '/src/assets/members/pardan.jpg',
+    photo: pardan,
     quote: 'Some things have to end in order for good things to begin.',
   },
   {
@@ -208,15 +207,15 @@ const members = [
     number: 22,
     name: 'Fardhas Oktaviandra',
     instagram: '@pardasss_',
-    photo: '/src/assets/members/pardas.jpg',
-    quote: 'duluan la, gek aku nyusul .',
+    photo: pardas,
+    quote: 'duluan la, gek aku nyusul.',
   },
   {
     id: 'member-23',
     number: 23,
     name: 'M. Raja Satria',
     instagram: '@sat_ria2408',
-    photo: '/src/assets/members/raja.jpeg',
+    photo: raja,
     quote: 'Sedikit drama, banyak karya.',
   },
   {
@@ -224,7 +223,7 @@ const members = [
     number: 24,
     name: 'Tora oryza pasha',
     instagram: '@oryza_pasha',
-    photo: '/src/assets/members/tora.jpg',
+    photo: tora,
     quote: 'wi wok de tok not only tok de tok.',
   },
   {
@@ -232,15 +231,15 @@ const members = [
     number: 25,
     name: 'Solahudin Wicaksono',
     instagram: '@adieeen_',
-    photo: '/src/assets/members/udin.jpeg',
+    photo: udin,
     quote: 'Jaga sholat.',
   },
   {
     id: 'member-26',
     number: 26,
-    name: 'muhammad wibisono ',
+    name: 'muhammad wibisono',
     instagram: '@ci_muchu',
-    photo: '/src/assets/members/wibi.jpeg',
+    photo: wibi,
     quote: 'Hidup tidak selalu berjalan sesuai rencana, tetapi kamu selalu bisa belajar menyesuaikan langkah.',
   },
   {
@@ -248,7 +247,7 @@ const members = [
     number: 27,
     name: 'muhamad farrel supriadi',
     instagram: '@upauwrell',
-    photo: '/src/assets/members/yad.jpg',
+    photo: yad,
     quote: 'hadapi dan nikmati.',
   },
   {
@@ -256,7 +255,7 @@ const members = [
     number: 28,
     name: 'M.Ashhabul Yamin',
     instagram: '@ashb_ul_yamin',
-    photo: '/src/assets/members/yamin.png',
+    photo: yamin,
     quote: 'baik buruk nya mohon maaf, Jika ada baiknya itu semua dari Allah, jika ada buruknya itu semua dari setan, dan saya hanya korban.',
   },
   {
@@ -264,7 +263,7 @@ const members = [
     number: 29,
     name: 'sulaiman zuhdi harahap',
     instagram: '@slmnzuhdii',
-    photo: '/src/assets/members/zuhdi.jpeg',
+    photo: zuhdi,
     quote: 'diam bukan berarti lemah, tapi lapar.',
   },
   {
@@ -272,7 +271,7 @@ const members = [
     number: 30,
     name: 'M. Syafiq Arifin.N',
     instagram: '@syafiqq.a_',
-    photo: '/src/assets/members/syafiq.jpeg',
+    photo: syafiq,
     quote: 'Orang yang kuat bukan mereka yang tidak pernah menangis, namun mereka yang tetap bangkit meski jatuh berkali-kali.',
   },
   {
@@ -280,7 +279,7 @@ const members = [
     number: 31,
     name: 'Muhammad Albar Al-Zaky',
     instagram: '@jaky.not.jek.2233',
-    photo: '/src/assets/members/jaki.jpeg',
+    photo: jaki,
     quote: 'Just Start, You will never be ready',
   },
   {
@@ -288,29 +287,17 @@ const members = [
     number: 32,
     name: 'Muhammad Indra Kusuma',
     instagram: '@mhmd_indra_',
-    photo: '/src/assets/members/indra.jpg',
+    photo: indra,
     quote: 'Teruslah Hidup.',
   },
   {
     id: 'member-33',
     number: 33,
-    name: 'M.Ichsan Firmansyah ',
+    name: 'M.Ichsan Firmansyah',
     instagram: '@michsanfirmnsyh',
-    photo: '/src/assets/members/ican.jpg',
-    quote: 'jadikan kegagalan sebagai bentuk dari pembelajaran .',
-  }
-
-
-
-
-  //{
-   // id: 'member-34',
-    //number: 33,
-    //name: 'Farhat Althaf Rifqi',
-    //photo: '/src/assets/members/farhat.jpg',
-   // memorial: true,
-    //memorialNote: 'Selamat jalan teman, semoga tenang di sisi-Nya. Terima kasih telah menjadi bagian dari perjalanan kita. Kenanganmu akan selalu hidup dalam hati kami.',
-  //},
+    photo: ican,
+    quote: 'jadikan kegagalan sebagai bentuk dari pembelajaran.',
+  },
 ];
 
 export default members;
